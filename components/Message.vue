1936079fac842bf0b77943ce85a7728a3f89574f
wkmg <wkmg.097@gmaill.com>
2020-02-27 00:49:34 +0900

    add: components ファイル追加
    追加 コンポーネント
    Messages / ChatForm

---
+++ components/Message.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="chats-container">
+    <div class="chat-container">
+      <div class="thumbnail-container">
+        <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" alt="">
+      </div>
+      <div class="message-container">
+        <div class="user-name">userName-testA</div>
+        <div class="message">message-testA</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.chat-container {
+  display: flex;
+  padding: 8px;
+}
+
+.thumbnail-container {
+  margin-right: 16px;
+}
+
+.thumbnail-container img {
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+}
+
+.user-name {
+  font-weight: bold;
+}
+</style>
